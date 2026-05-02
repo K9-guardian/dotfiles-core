4363ee26bef352828084a4a0b02954320682bedd
Krishna Girkar <girkar.km@gmail.com>
2026-05-01 21:17:01 -0700

    treesitter is amazing

--- roles/core_neovim/files/config/lua/plugins/configs/treesitter.lua
+++ roles/core_neovim/files/config/lua/plugins/configs/treesitter.lua
@@ -1,4 +1,3 @@
----@diagnostic disable-next-line: missing-fields
 require("nvim-treesitter.configs").setup {
    ensure_installed = {
       "clojure",
@@ -8,8 +7,62 @@ require("nvim-treesitter.configs").setup {
       "javascript",
       "prolog",
       "python",
+      "query",
       "racket",
       "rust",
    },
-   highlight = { enable = true }
+   highlight = { enable = true },
+
+   textobjects = {
+      move = {
+         enable = true,
+         set_jumps = true,
+         goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]c"] = "@class.outer",
+         },
+         goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]C"] = "@class.outer",
+         },
+         goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[c"] = "@class.outer",
+         },
+         goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[C"] = "@class.outer",
+         },
+      },
+
+      -- TODO: Use mini.ai for this.
+      --       I want to cover all the major programming constructs with queries.
+      select = {
+         -- enable = true,
+         lookahead = true,
+         keymaps = {
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+         },
+         include_surrounding_whitespace = true,
+      },
+
+      swap = {
+         enable = true,
+         lookahead = true,
+         swap_next = {
+            ["]a"] = "@parameter.inner",
+         },
+         swap_previous = {
+            ["[a"] = "@parameter.inner",
+         },
+      },
+   },
 }
+
+local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
