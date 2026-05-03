bb0cb14b5058afbc0a99e0232382cabf2642e17b
Krishna Girkar <girkar.km@gmail.com>
2026-05-03 16:13:09 -0700

    don't mess with my path

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -LsSf https://astral.sh/uv/install.sh | sh
+curl -LsSf https://astral.sh/uv/install.sh | env UV_NO_MODIFY_PATH=1 sh
 
 ANSIBLE_VENV="$HOME/.local/ansible-venv"
 uv venv --python 3.14.4 "$ANSIBLE_VENV"
