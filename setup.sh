1f352d972ae051e59842ed41ea63d73f2a224bcc
Krishna Girkar <kgirkar@arista.com>
2026-07-02 19:47:13 -0700

    fix setup to not need path

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | env UV_NO_MODIFY_PATH=1 sh
 
 ANSIBLE_VENV="$HOME/.local/ansible-venv"
-uv venv --python 3.14.4 "$ANSIBLE_VENV"
-uv pip install ansible==13.6.0 --python "$ANSIBLE_VENV/bin/python"
+$HOME/.local/bin/uv venv --python 3.14.4 "$ANSIBLE_VENV"
+$HOME/.local/bin/uv pip install ansible==13.6.0 --python "$ANSIBLE_VENV/bin/python"
