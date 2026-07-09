a8fd10a9a32de9de299cd71930e9ca8f6391c996
Krishna Girkar <kgirkar@arista.com>
2026-07-09 01:41:30 +0000

    keep comments for treesitter

--- roles/neovim/files/config/lua/plugins/vanilla_plus.lua
+++ roles/neovim/files/config/lua/plugins/vanilla_plus.lua
@@ -20,6 +20,12 @@ vim.pack.add({
    'https://github.com/stevearc/oil.nvim',
 })
 
+-- vim.pack.add({
+--    'https://github.com/nvim-treesitter/nvim-treesitter',
+--    'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
+--    'https://github.com/HiPhish/rainbow-delimiters.nvim',
+-- })
+
 require("plugins.configs.tokyonight")
 require("plugins.configs.asterisk")
 require("plugins.configs.surround")
